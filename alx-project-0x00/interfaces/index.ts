6b3ce134b684a85e3fa7ccf936087e15417a971c
OgborFaith <faithogbor23@gmail.com>
2025-11-12 17:48:16 +0100

    corrections has been made

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -4,8 +4,9 @@ export interface PillProps {
 
 export interface ButtonProps {
   title: string;
+   styles?: string;
   size?: "small" | "medium" | "large";
-  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-lg";
   onClick?: () => void;
   className?: string;
 }
