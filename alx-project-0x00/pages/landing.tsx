c7e8d63695bd1634a7744680c45fc2423a0d6f55
OgborFaith <faithogbor23@gmail.com>
2025-11-12 17:54:48 +0100

    changes made

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -5,14 +5,15 @@ const Landing: React.FC = () => {
  return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
-      <Card />
+      <Card /> ["rounded-full"]
       <div className="space-y-4 mt-4">
       <Button title="Small Rounded" size="small" shape="rounded-sm" />
       <Button title="Medium Rounded" size="medium" shape="rounded-md" />
       <Button title="Large Rounded Full" size="large" shape="rounded-lg" />
-      <Button title="Custom Style" className="bg-green-500" />
+      <Button title="Custom Style" className="bg-green-500"  />
     </div>
     </div>
+    
     
   )
 }
